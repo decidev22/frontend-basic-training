c583cd8a10959624d203eb83df3ac9b4c3fd0d61
decidev22 <hamento22@gmail.com>
2023-10-05 23:51:54 +1300

    Q14

--- src/components/13 ForwardRef/question.tsx
+++ src/components/13 ForwardRef/question.tsx
@@ -1,14 +1,29 @@
 // TODO: Modify the code so that on page load, the second MyCustomInput input field receives focus
 // TODO: Add typescript to this file
 
-const MyCustomInput = ({ label }) => (
-  <>
-    <label>{label}</label>
-    <input></input>
-  </>
+import { useEffect, useRef, forwardRef } from "react";
+
+// Q - usage of forwardRef
+// const FancyButton = React.forwardRef((props, ref) => (
+//   <button ref={ref} className="FancyButton">
+//     {props.children}
+//   </button>
+// ));
+
+const MyCustomInput = forwardRef<HTMLInputElement, { label: string }>(
+  ({ label }, ref) => (
+    <>
+      <label>{label}</label>
+      <input ref={ref}></input>
+    </>
+  )
 );
 
 export const ForwardRefQ = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    ref.current!.focus();
+  }, []);
   return (
     <>
       <ul>
@@ -16,7 +31,10 @@ export const ForwardRefQ = () => {
           <MyCustomInput label="Customized 1"></MyCustomInput>
         </li>
         <li>
-          <MyCustomInput label="Second Input"></MyCustomInput>
+          <MyCustomInput
+            label="Second Input"
+            ref={ref}
+          ></MyCustomInput>
         </li>
       </ul>
     </>
--- src/components/14 Checkpoint2-Departure-Search/question.tsx
+++ src/components/14 Checkpoint2-Departure-Search/question.tsx
@@ -1,16 +1,85 @@
 //TODO: Add Typescript to this Search Result component
 
+import { forwardRef, useEffect, useRef, useState } from "react";
+
 //TODO: Create a custom hook, that accepts an array of routes as input
-const useSearchRoute = (routes) => {
-  return {};
+// Q what is hook and how is it different from handler
+const useSearchRoute = (routes: SearchResultProps[]) => {
+  // searches given data... {routes}
+  const [name, setName] = useState("");
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [results, setResults] = useState(routes);
+
+  const triggerSearch: React.MouseEventHandler<
+    HTMLButtonElement
+  > = () => {
+    // triggers Search?
+    const search = routes.filter(
+      (route) =>
+        (route.name === name || name === "") &&
+        (route.route.origin === origin || origin === "") &&
+        (route.route.destination === destination ||
+          destination === "")
+    );
+    setResults(search);
+  };
+  return {
+    results,
+    setName,
+    setOrigin,
+    setDestination,
+    triggerSearch,
+  };
 };
 
 //TODO: Complete the SearchBox component
-const SearchBox = ({}) => {
-  return <input></input>;
-};
 
-const SearchResult = ({ results }) => {
+interface SearchBoxProps {
+  placeholder: string;
+  searchItem: React.Dispatch<React.SetStateAction<string>>;
+}
+
+// What is this!
+const SearchBox = forwardRef<HTMLInputElement, SearchBoxProps>(
+  ({ placeholder, searchItem }, ref) => {
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (
+      e
+    ) => {
+      searchItem(e.target.value);
+    };
+    return (
+      <input
+        ref={ref}
+        placeholder={placeholder}
+        onChange={handleChange}
+      ></input>
+    );
+  }
+);
+
+// what are we showing??
+// {
+//   name: "Roam 1",
+//   route: {
+//     origin: "Earth",
+//     destination: "Mars",
+//   },
+// },
+interface SearchResultProps {
+  name: string;
+  route: {
+    origin: string;
+    destination: string;
+  };
+}
+// results probably takes in an Array type of Objects that has name origin and destination.
+// but that is also what the routes variable has...!
+const SearchResult = ({
+  results,
+}: {
+  results: SearchResultProps[];
+}) => {
   return (
     <ul>
       {results.map((result) => (
@@ -25,15 +94,34 @@ const SearchResult = ({ results }) => {
 // TODO: On page load, set focus to the first search box
 // TOTO: Add functionality so that when the type search terms into the three different
 // search boxes, the routes are filtered and the correct result is displayed
-const DepartureSearch = ({ routes }) => {
-  const { results } = useSearchRoute(routes);
+const DepartureSearch = ({
+  routes,
+}: {
+  routes: SearchResultProps[];
+}) => {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  }, []);
+  const {
+    results,
+    setName,
+    setOrigin,
+    setDestination,
+    triggerSearch,
+  } = useSearchRoute(routes);
 
   return (
     <div>
-      <SearchBox placeholder="Name" />
-      <SearchBox placeholder="Origin" />
-      <SearchBox placeholder="Destination" />
-      <button>Submit</button>
+      <SearchBox placeholder="Name" searchItem={setName} ref={ref} />
+      <SearchBox placeholder="Origin" searchItem={setOrigin} />
+      <SearchBox
+        placeholder="Destination"
+        searchItem={setDestination}
+      />
+      <button onClick={triggerSearch}>Search</button>
       <SearchResult results={results} />
     </div>
   );
